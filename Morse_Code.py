9fd90171e5d3dd120c4bac622a608475427b5cc9
Sravan Chittupalli <54674671+SravanChittupalli@users.noreply.github.com>
2019-09-01 23:32:21 +0530

    Add files via upload

---
+++ Morse_Code.py
@@ -0,0 +1,50 @@
+MORSE_CODE_DICT = { 'A':'.-',   'B':'-...', 
+                    'C':'-.-.', 'D':'-..',  'E':'.', 
+                    'F':'..-.', 'G':'--.',  'H':'....', 
+                    'I':'..',   'J':'.---', 'K':'-.-', 
+                    'L':'.-..', 'M':'--',   'N':'-.', 
+                    'O':'---',  'P':'.--.', 'Q':'--.-', 
+                    'R':'.-.',  'S':'...',  'T':'-', 
+                    'U':'..-',  'V':'...-', 'W':'.--', 
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..', 
+                    '1':'.----','2':'..---','3':'...--', 
+                    '4':'....-','5':'.....','6':'-....', 
+                    '7':'--...','8':'---..','9':'----.', 
+                    '0':'-----',', ':'--..--', '.':'.-.-.-', 
+                    '?':'..--..','/':'-..-.', '-':'-....-', 
+                    '(':'-.--.', ')':'-.--.-' } 
+decodedword = ''
+
+
+def encode(string , final):
+    if (string == ' '):
+        final = final + '  '
+    else:
+        encoded = MORSE_CODE_DICT[string]
+        final = final + encoded + ' '
+    return final
+
+def decode(encodedword):
+    for key , value in MORSE_CODE_DICT.items():
+        if(value == encodedword):
+            return (str(decodedword) + str(key))
+    
+    
+if __name__ == '__main__':
+    message = input("Enter The Sentence To Be Converted To Morse Code : ")
+    final = ''
+    length = len(message)
+    for i in message :
+        final = encode(i , final)
+    print(final)
+    
+
+    encodedmess = input("Enter The Encoded Message To Be Decoded : ")
+    encodedwords = encodedmess.split('  ')
+    
+    for i in encodedwords:
+        encodedletters = i.split(' ')
+        for j in encodedletters:
+            decodedword = decode(j)
+        decodedword = str(decodedword) + ' '
+    print(decodedword)
